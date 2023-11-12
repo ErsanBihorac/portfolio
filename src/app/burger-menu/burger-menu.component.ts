666ae35668fadfe921b7b6266a28b3ce79fb29b0
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-11-12 23:40:01 +0100

    burgermenu can redirect the user to git,linkedin, and to other parts of the website

--- src/app/burger-menu/burger-menu.component.ts
+++ src/app/burger-menu/burger-menu.component.ts
@@ -7,6 +7,51 @@ import { timeout } from 'rxjs';
   styleUrls: ['./burger-menu.component.scss']
 })
 export class BurgerMenuComponent {
+  burger_images = [
+    "../../assets/img/animations/burger-menu.png",
+    "../../assets/img/animations/burgermenu1.png",
+    "../../assets/img/animations/burgermenu2.png",
+    "../../assets/img/animations/burgermenu3.png",
+    "../../assets/img/animations/burgermenu4.png"
+  ];
+
+  goTo(id: string) {
+    this.closeBurgerMenu();
+    document.getElementById(id)?.scrollIntoView();
+    this.burgerAnimationReverse();
+  }
+
+
+  burgerAnimation() {
+    let img = document.getElementById('img-burger') as HTMLImageElement;
+    for (let i = 0; i < this.burger_images.length; i++) {
+      let burger_images = this.burger_images[i];
+      setTimeout(() => {
+        img.src = burger_images;
+      }, 75 * i);
+    }
+  }
+
+  burgerAnimationReverse() {
+    let img = document.getElementById('img-burger') as HTMLImageElement;
+    this.burger_images.reverse();
+    for (let i = 0; i < this.burger_images.length; i++) {
+      let burger_images = this.burger_images[i];
+      setTimeout(() => {
+        img.src = burger_images;
+      }, 75 * i);
+    }
+    this.burger_images.reverse();
+  }
+
+  closeBurgerMenu() {
+    document.getElementById('about-me-page')?.classList.remove('d-none');
+    document.getElementById('main-page')?.classList.remove('d-none');
+    document.getElementById('my-skills-page')?.classList.remove('d-none');
+    document.getElementById('portfolio-page')?.classList.remove('d-none');
+    document.getElementById('footer')?.classList.remove('d-none');
+    document.getElementById('burger-menu-page')?.classList.add('d-none');
+  }
 
   headerAnimation(container: string, image: string) {
     let superiorcontainer = document.getElementById(container);
