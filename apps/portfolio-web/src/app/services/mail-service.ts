00881e6e917577c0b0f25320c4804841f357272e
ersan <ersan@MacBook-Air-von-ersan.local>
2026-06-29 10:27:14 +0200

    fix: fixed import of environment apiUrl

--- apps/portfolio-web/src/app/services/mail-service.ts
+++ apps/portfolio-web/src/app/services/mail-service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { email } from '../interfaces/email.interface';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
